272a7f3d4dd63ff68b39c1f5c4b70148b2c10ac7
marie-phu-qui <marie.nguyen.phu.qui@gmail.com>
2018-04-23 13:48:45 +1200

    sound working

--- minesweeper.js
+++ minesweeper.js
@@ -2,6 +2,11 @@ document.addEventListener('DOMContentLoaded', startGame)
 
 var board = {};
 
+
+function releaseMines(){
+  prompt('How many possums do you want to release : ')
+}
+
 function createBoard(){
 board = {
   cells : [],
@@ -16,59 +21,12 @@ var boardSize = 9;
         hidden: true,
         surroundingMines: 0,
         isMine : (Math.random()<0.3),
-      });
+    });
     }
   }
 return board;
 }
 
-  // Don't remove this function call: it makes the game work!
-function startGame () {
-  createBoard();
-  for (var i=0; i<board['cells'].length;i++){
-    countSurroundingMines(board.cells[i]);
-    board.cells[i].surroundingMines = countSurroundingMines(board.cells[i]);
-  }
-  document.addEventListener('click', checkForWin);
-  document.addEventListener('contextmenu', checkForWin);
-  lib.initBoard()
-}
-
-// Define this function to look for a win condition:
-//
-// 1. Are all of the cells that are NOT mines visible?
-// 2. Are all of the mines marked?
-function checkForWin () {
-  for (var i = 0; i <board.cells.length; i++) {
-    if (board.cells[i].isMine && (!board.cells[i].isMarked)) {
-      return;
-    }
-    else if ((!board.cells[i].isMine) && board.cells[i].hidden) {
-      return;
-    }
-  }
-  lib.displayMessage('You win!');
-}
-
-// Restart game function 
-// Resetting the board to its default state. 
-// Put classes back the way they were at the start, and re-initialise the global board object.
-
-function restartGame(){
-  location.reload();
-};
-
-  // You can use this function call to declare a winner (once you've
-  // detected that they've won, that is!)
-  //   lib.displayMessage('You win!')
-
-// Define this function to count the number of mines around the cell
-// (there could be as many as 8). You don't have to get the surrounding
-// cells yourself! Just use `lib.getSurroundingCells`: 
-
-
-// It will return cell objects in an array. You should loop through 
-// them, counting the number of times `cell.isMine` is true.
 function countSurroundingMines(cell) {
   var surrounding = lib.getSurroundingCells(cell.row, cell.col);
   var count = 0;
@@ -82,3 +40,63 @@ function countSurroundingMines(cell) {
   }
   return count
 }
+
+function startGame () {
+  createBoard();
+  for (var i=0; i<board['cells'].length;i++){
+    countSurroundingMines(board.cells[i]);
+    board.cells[i].surroundingMines = countSurroundingMines(board.cells[i]);
+  }
+  document.addEventListener('click', checkForWin);
+  document.addEventListener('click', checkForLost);
+  document.addEventListener('contextmenu', checkForWin);
+  lib.initBoard()
+}
+
+function checkForWin () {
+  for (var i = 0; i <board.cells.length; i++) {
+    if (board.cells[i].isMine && (!board.cells[i].isMarked)) {
+      return;
+    }
+    else if ((!board.cells[i].isMine) && board.cells[i].hidden) {
+      return;
+    }
+  }
+  lib.displayMessage('You win!');
+  playSound('sound-win');
+}
+
+function checkForLost () {
+  for (var i = 0; i <board.cells.length; i++) {
+    if (board.cells[i].isMine && !board.cells[i].hidden) {
+      playSound('sound-lost');
+    }
+  }
+};
+
+function restartGame(){
+  function clearBoard(){
+    for (var i = 0; i < board.cells.length; i++){
+      document.getElementsByClassName('board')[0].innerHTML = '';
+    }
+  };
+  clearBoard();
+  startGame();
+  stopSound();
+};
+
+
+// Play sound 
+function playSound(sound){
+  var sound = document.getElementById('sound');
+  sound.play();
+}
+
+
+function stopSound(sound){
+  var sound = document.getElementById('sound');
+  sound.pause();
+}
+
+
+
